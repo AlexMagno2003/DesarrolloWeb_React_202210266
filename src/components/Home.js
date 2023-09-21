834a6374f1657446c3dfe054d06106dd7341ae74
AlexMagno2003 <jorozcoalfaro@gmail.com>
2023-09-20 23:16:50 -0600

    SEGUNDO COMMIT

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,41 +1,62 @@
 import React, { useState, useEffect } from 'react';
 import Modal from 'react-modal';
 import CrearPublicacionModal from './CrearPublicacionModal';
+import $ from 'jquery';
 
 
 function Home({ usuario, registroAcademico }) {
   const [publicaciones, setPublicaciones] = useState([]);
   const [cursos, setCursos] = useState([]);
   const [catedraticos, setCatedraticos] = useState([]);
+  const [cursoSeleccionado, setCursoSeleccionado] = useState('');
+  const [catedraticoSeleccionado, setCatedraticoSeleccionado] = useState('');
   const [comentarioInput, setComentarioInput] = useState('');
-    const [errorResponse, setErrorResponse] = useState("");
-    const [modalIsOpen, setModalIsOpen] = useState(false);
-
+  const [errorResponse, setErrorResponse] = useState('');
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
   useEffect(() => {
     // Realiza una solicitud para obtener las publicaciones con comentarios
-    fetch('http://localhost:4000/publicaciones')
-      .then((response) => response.json())
-      .then((data) => setPublicaciones(data))
-      .catch((error) => console.error('Error al obtener las publicaciones:', error));
-  }, []);
-    
-    useEffect(() => {
-    fetch('http://localhost:4000/cursos')
-      .then((response) => response.json())
-      .then((data) => setCursos(data))
-      .catch((error) => console.error('Error al obtener los cursos:', error));
-    }, []);
-    
-    useEffect(() => {
-    fetch('http://localhost:4000/catedraticos')
-      .then((response) => response.json())
-      .then((data) => setCatedraticos(data))
-      .catch((error) => console.error('Error al obtener los cursos:', error));
-    }, []);
+    fetchPublicaciones();
 
+    // Realiza una solicitud para obtener los cursos y catedráticos
+    fetchCursos();
+    fetchCatedraticos();
+  }, [cursoSeleccionado, catedraticoSeleccionado]);
 
-    function formatearFecha(fecha) {
+  const fetchPublicaciones = () => {
+    // Realiza la solicitud AJAX para obtener las publicaciones con filtros
+    $.ajax({
+      url: `http://localhost:4000/publicaciones?curso=${cursoSeleccionado}&catedratico=${catedraticoSeleccionado}`,
+      method: 'GET',
+      dataType: 'json',
+      success: (data) => setPublicaciones(data),
+      error: (error) => console.error('Error al obtener las publicaciones:', error),
+    });
+  };
+
+  const fetchCursos = () => {
+    // Realiza la solicitud AJAX para obtener los cursos
+    $.ajax({
+      url: 'http://localhost:4000/cursos',
+      method: 'GET',
+      dataType: 'json',
+      success: (data) => setCursos(data),
+      error: (error) => console.error('Error al obtener los cursos:', error),
+    });
+  };
+
+  const fetchCatedraticos = () => {
+    // Realiza la solicitud AJAX para obtener los catedráticos
+    $.ajax({
+      url: 'http://localhost:4000/catedraticos',
+      method: 'GET',
+      dataType: 'json',
+      success: (data) => setCatedraticos(data),
+      error: (error) => console.error('Error al obtener los catedráticos:', error),
+    });
+  };
+
+  const formatearFecha = (fecha) => {
     const options = {
       year: 'numeric',
       month: '2-digit',
@@ -43,50 +64,82 @@ function Home({ usuario, registroAcademico }) {
       hour: '2-digit',
       minute: '2-digit',
     };
-
     return new Date(fecha).toLocaleDateString(undefined, options);
-    }
-    
-    const handleLogOut = (event) =>
-    {
-        localStorage.removeItem('sessionToken');
-        window.location.reload();
-      }
+  };
+
+  const handleLogOut = () => {
+    localStorage.removeItem('sessionToken');
+    window.location.reload();
+  };
+
   const handleComentarioSubmit = async (publicacionId) => {
-    
     const nuevoComentario = {
       id_publicacion: publicacionId,
-      texto: comentarioInput
+      texto: comentarioInput,
     };
 
     const response = await registrarComentario('http://localhost:4000/registroComentario', nuevoComentario);
     if (response.error) {
-          setErrorResponse(response.error);
-        } else {
-          console.log('Comentario registrado con éxito');
-            window.location.reload();
-        }
-    };
-    
- async function registrarComentario(url = '', data = {}) {
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        });
-        return response.json();
+      setErrorResponse(response.error);
+    } else {
+      console.log('Comentario registrado con éxito');
+      window.location.reload();
     }
+  };
+
+  const registrarComentario = async (url = '', data = {}) => {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+    return response.json();
+  };
 
   return (
+    <div>
+      <h1>
+        Bienvenido {usuario} {registroAcademico}
+      </h1>
+
+      <button onClick={handleLogOut}>Cerrar Sesión</button>
+      <h2>Publicaciones</h2>
+      <button onClick={() => setModalIsOpen(true)}>Crear Publicación</button>
+      <h2>Filtrar Publicaciones</h2>
       <div>
-          <h1>Bienvenido {usuario} {registroAcademico}</h1> 
-          
-          <button onClick={() => handleLogOut()}>Cerrar Sesión</button>
-          <h2>Publicaciones</h2>
-          <button onClick={() => setModalIsOpen(true)}>Crear Publicación</button>
-          <Modal
+        <label>Curso:</label>
+        <select
+          value={cursoSeleccionado}
+          onChange={(e) => setCursoSeleccionado(e.target.value)}
+          required
+        >
+          <option value="">Selecciona un curso</option>
+          {cursos.map((curso) => (
+            <option key={curso.id_curso} value={curso.id_curso}>
+              {curso.nombre}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div>
+        <label>Catedrático:</label>
+        <select
+          value={catedraticoSeleccionado}
+          onChange={(e) => setCatedraticoSeleccionado(e.target.value)}
+          required
+        >
+          <option value="">Selecciona un catedrático</option>
+          {catedraticos.map((catedratico) => (
+            <option key={catedratico.id_catedratico} value={catedratico.id_catedratico}>
+              {catedratico.nombre}
+            </option>
+          ))}
+        </select>
+      </div>
+      <button onClick={fetchPublicaciones}>Filtrar</button>
+      <Modal
         isOpen={modalIsOpen}
         onRequestClose={() => setModalIsOpen(false)}
         contentLabel="Crear Publicación"
@@ -99,33 +152,37 @@ function Home({ usuario, registroAcademico }) {
           catedraticos={catedraticos} // Pasa los catedráticos obtenidos desde el servidor
         />
       </Modal>
-      {publicaciones.map((publicacion) => (
-        <div key={publicacion.id_publicacion} className="card-publicacion"> 
-          <h3>
-            Autor: {publicacion.nombre_usuario} {publicacion.apellidos_usuario} - {formatearFecha(publicacion.fecha_creacion)}
-          </h3>
-          {!!publicacion.nombre_curso && <p><strong>Curso:</strong>  {publicacion.nombre_curso}</p>}
-          {!!publicacion.nombre_catedratico && <p><strong>Catedrático:</strong>  {publicacion.nombre_catedratico}</p>}
-          <p><strong>Mensaje:</strong> {publicacion.mensaje_publicacion}</p>
+      {publicaciones.length === 0 ? (
+        <p>No hay publicaciones con estos cursos o catedráticos.</p>
+      ) : (
+        publicaciones.map((publicacion) => (
+          <div key={publicacion.id_publicacion} className="card-publicacion">
+            <h3>
+              Autor: {publicacion.nombre_usuario} {publicacion.apellidos_usuario} - {formatearFecha(publicacion.fecha_creacion)}
+            </h3>
+            {!!publicacion.nombre_curso && <p><strong>Curso:</strong> {publicacion.nombre_curso}</p>}
+            {!!publicacion.nombre_catedratico && <p><strong>Catedrático:</strong> {publicacion.nombre_catedratico}</p>}
+            <p><strong>Mensaje:</strong> {publicacion.mensaje_publicacion}</p>
 
-          {/* Input para publicar comentarios */}
-          <input
-            type="text"
-            value={comentarioInput}
-            onChange={(e) => setComentarioInput(e.target.value)}
-            placeholder="Escribe un comentario..."
-          />
-          <button onClick={() => handleComentarioSubmit(publicacion.id_publicacion)}>Publicar Comentario</button>
+            {/* Input para publicar comentarios */}
+            <input
+              type="text"
+              value={comentarioInput}
+              onChange={(e) => setComentarioInput(e.target.value)}
+              placeholder="Escribe un comentario..."
+            />
+            <button onClick={() => handleComentarioSubmit(publicacion.id_publicacion)}>Publicar Comentario</button>
 
-              {/* Lista de comentarios */}
+            {/* Lista de comentarios */}
             <h3>Comentarios</h3>
-          <ol>
-            {publicacion.comentarios.map((comentario) => (
-              <li key={comentario.id_comentario} className="card-comentario">{comentario.texto}</li>
-            ))}
-          </ol>
-        </div>
-      ))}
+            <ol>
+              {publicacion.comentarios.map((comentario) => (
+                <li key={comentario.id_comentario} className="card-comentario">{comentario.texto}</li>
+              ))}
+            </ol>
+          </div>
+        ))
+      )}
     </div>
   );
 }
